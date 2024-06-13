0863825e363e948190aadbb8b33c96dab3f7fe8f
gunadevandesing <gunadevandesing@gmail.com>
2024-06-13 16:33:37 +0000

    mfe-app-02

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  port: 3000,
+  plugins: [react(), federation({
+    name:"mfe-app01",
+    filename: "remoteEntry.js",
+    exposes: {
+      "./App2": "./src/App.jsx",
+    },
+    remotes:{
+      "mfe-app02": "https://mfe-app-01.vercel.app/assets/remoteEntry.js",
+    },
+    shared: ["react", "react-dom"],
+  })],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+})
